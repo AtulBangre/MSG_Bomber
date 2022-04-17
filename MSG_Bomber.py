6e8a7bdf8d56081c38b01b759abd329ca08631e5
AtulBangre <97684560+AtulBangre@users.noreply.github.com>
2022-04-17 11:02:13 +0530

    Update MSG_Bomber.py

--- MSG_Bomber.py
+++ MSG_Bomber.py
@@ -1,8 +1,12 @@
 import pyautogui as pg
 import time
-time.sleep(8)
-for i in range(500):
-    pg.write("Soja Bhosdike")
+times = int(input("Enter countdown-\t"))
+num = int(input("Enter number for how many \ntimes you should send msg-\t"))
+msg = input("Type msg=\t")
+for i in reversed(range(times)):
+    print(f"program start in {i+1}")
+    time.sleep(1)
+for i in range(num):
+    pg.write(msg)
     pg.press("Enter")
-
 
